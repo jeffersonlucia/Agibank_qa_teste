d0fd0100f9632aee1aa49c8566d5087e19a1d043
Jefferson Lucia <jefferson.rlucia@gmail.com>
2024-05-05 08:39:45 -0300

    1.0

--- cypress/e2e/SearchButton.cy.js
+++ cypress/e2e/SearchButton.cy.js
@@ -1,7 +1,8 @@
 it('Deve acessar portal da Blog Agi', () => { 
     cy.viewport(1920, 1080)
         .visit('https://blogdoagi.com.br/')
-        .get('#site-branding > .custom-logo-wrap > .custom-logo-link > .custom-logo')
+        cy.get('.site-header-above-section-left > .ast-builder-layout-element > .site-branding > .site-logo-img > .custom-logo-link > .custom-logo')
+        .should('be.exist')
 })
 
 
@@ -9,118 +10,112 @@ it('Deve acessar portal da Blog Agi', () => {
 it('Deve acionar a lupa de pesquisa', () => {
     cy.viewport(1920, 1080)
         .visit('https://blogdoagi.com.br/')
-        .get('#search-open')
+        cy.get('.site-header-above-section-right > .ast-builder-layout-element')
         .click()
 
-    //o painel suspenso de pesquisa deverá ser localizado na tela
-    cy.get('.desktop-search > .search-form > label > .search-field').should('be.visible')
+    //o portlet de pesquisa deverá ser localizado na tela
+    cy.get('#search-field')
+    .should('be.visible')
 })
 
 
 
 it('Deve esconder a lupa de pesquisa', () => {
     cy.viewport(1920, 1080)
-        .visit('https://blogdoagi.com.br/')
-        .get('#search-open')
+        cy.visit('https://blogdoagi.com.br/')
+        cy.get('.site-above-header-wrap')
         .click()
 
-    //o painel suspenso de pesquisa deverá ser localizado na tela
+    //o portlet de pesquisa não deverá ser localizado na tela
     cy.get('.desktop-search > .search-form > label > .search-field')
-        .should('be.visible')
-
-    cy.get('#search-open').click()
-    
-    //o painel suspenso de pesquisa não deverá ser localizado na tela
-    cy.get('.desktop-search > .search-form > label > .search-field')
-        .should('not.be.visible')
+        .should('not.be.exist')
 })
 
 
 it('Deve atualizar a pagina com a lupa de pesquisa acionada', () => {
     cy.viewport(1920, 1080)
-        .visit('https://blogdoagi.com.br/')
-        .get('#search-open').click()
-    
-    //o painel suspenso de pesquisa deverá ser localizado na tela
-    cy.get('.desktop-search > .search-form > label > .search-field')
-        .should('be.visible')
-        .reload()
+   cy.visit('https://blogdoagi.com.br/')
+    cy.get('.site-above-header-wrap')
+    .click()
 
-    //o painel suspenso de pesquisa não deverá ser localizado na tela
-    cy.get('.desktop-search > .search-form > label > .search-field')
-        .should('not.be.visible')  
+    cy.reload()
+    
+     //o portlet de pesquisa não deverá ser localizado na tela
+     cy.get('.desktop-search > .search-form > label > .search-field')
+     .should('not.be.exist')
+
+
 })
 
 it('Deve realizar uma busca vazia no campo de busca', () => {
     cy.viewport(1920, 1080)
-        .visit('https://blogdoagi.com.br/')
-        .get('#search-open')
-        .click()
-    
-    //o painel suspenso de pesquisa deverá ser localizado na tela
-    cy.get('.desktop-search > .search-form > label > .search-field')
-        .should('be.visible')
-        .get('.desktop-search > .search-form > .search-submit')
-        .click()
-        .get('span > button')
-        .should('be.visible')
+    cy.visit('https://blogdoagi.com.br/')
+    cy.get('.site-header-above-section-right > .ast-builder-layout-element')
+    .click()
+
+    cy.get('#search-field')
+.type('{enter}')
+
+//resultado da pesquisa
+cy.get('.ast-archive-description')
+.contains('Resultados encontrados para:')
 })
 
 it('Deve realizar uma busca com valores existentes no campo de busca', () => {
     cy.viewport(1920, 1080)
-        .visit('https://blogdoagi.com.br/')
-        .get('#search-open').click()
+    cy.visit('https://blogdoagi.com.br/')
+    cy.get('.site-header-above-section-right > .ast-builder-layout-element')
+    .click()
+
+    cy.get('#search-field')
+    .type('Como fazer agendamento no CRAS: confira guia completo')
+    .type('{enter}')
     
-    //o painel suspenso de pesquisa deverá ser localizado na tela
-    cy.get('.desktop-search > .search-form > label > .search-field')
-        .should('be.visible')
-        .get('.desktop-search > .search-form > label > .search-field')
-        .type('teste')
-        .get('.desktop-search > .search-form > .search-submit')
-        .click()
-    cy.contains('teste')
-        .should('be.visible')
+    //obtem elemento da busca
+    cy.get('.post-content')
+    .contains('Como fazer agendamento no CRAS: confira guia completo')
     
 })
 
 
 it('Deve realizar uma busca com valores inexistentes no campo de busca', () => {
-    cy.viewport(1920, 1080)
-        .visit('https://blogdoagi.com.br/')
-        .get('#search-open').click()
+        cy.viewport(1920, 1080)
+        cy.visit('https://blogdoagi.com.br/')
+        cy.get('.site-header-above-section-right > .ast-builder-layout-element')
+        .click()
     
-    //o painel suspenso de pesquisa deverá ser localizado na tela
-    cy.get('.desktop-search > .search-form > label > .search-field')
-        .should('be.visible')
-    cy.get('.desktop-search > .search-form > label > .search-field')
-        .type('123456abcdef123')
-    cy.get('.desktop-search > .search-form > .search-submit').click()
-    cy.contains('Nenhum resultado').should('be.visible')
+        cy.get('#search-field')
+        .type('guarda sol')
+        .type('{enter}')
+        
+        //obtem elemento da busca
+        cy.get('.no-results')
+        .contains('Lamentamos, mas nada foi encontrado para sua pesquisa, tente novamente com outras palavras.')
+        
 })
 
-it('Deve realizar uma busca com valores inexistentes no campo de busca', () => {
+it('Deve realizar uma busca com valores existentes no campo de busca após uma busca inexistente', () => {
     cy.viewport(1920, 1080)
-        .visit('https://blogdoagi.com.br/')
-        .get('#search-open').click()
+    cy.visit('https://blogdoagi.com.br/')
+    cy.get('.site-header-above-section-right > .ast-builder-layout-element')
+    .click()
 
-    //o painel suspenso de pesquisa deverá ser localizado na tela
-    cy.get('.desktop-search > .search-form > label > .search-field').should('be.visible')
-    cy.get('.desktop-search > .search-form > label > .search-field').type('123456abcdef123')
-    cy.get('.desktop-search > .search-form > .search-submit').click()
+    cy.get('#search-field')
+    .type('guarda sol')
+    .type('{enter}')
+    
+    //obtem elemento da busca
+    cy.get('.no-results')
+    .contains('Lamentamos, mas nada foi encontrado para sua pesquisa, tente novamente com outras palavras.')
+
+    //informa valor de busca no novo portlet de busca
+    cy.get('.widget > .search-form > label > #search-field')
+    .type('Como fazer agendamento no CRAS: confira guia completo')
+    .type('{enter}')
+
+    
+    
 })
 
-it('Deve realizar uma busca com valores inexistentes no campo de busca', () => {
-    cy.viewport(1920, 1080)
-        .visit('https://blogdoagi.com.br/')
-        .get('#search-open')
-        .click()
-})
 
-it('Deve excluir valores de busca anteriores', () => {
-    cy.viewport(1920, 1080)
-        .visit('https://blogdoagi.com.br/')
-        .get('#search-open')
-        .click()
-        .get('.desktop-search > .search-form > label > .search-field')
-        .clear()
-})
+
